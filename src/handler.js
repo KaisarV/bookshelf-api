0ac56d2e9e2e3ef29595907f749d8ebfd3ce9da3
KaisarV <kaisar.valentino123@gmail.com>
2022-06-23 21:41:15 +0700

    first commit

---
+++ src/handler.js
@@ -0,0 +1,16 @@
+const { nanoid } = require('nanoid');
+
+const addBookHandler = (request, h) => {
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  const id = nanoid(16);
+};
