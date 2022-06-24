43f824b5642b0b01ff5a0ee795ce5ac7b5c7aba0
KaisarV <kaisar.valentino123@gmail.com>
2022-06-24 15:50:23 +0700

    get book detail by id

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,8 @@
-const { addBookHandler, getAllBooksHandler } = require('./handler');
+const {
+  addBookHandler,
+  getAllBooksHandler,
+  getBookDetailById,
+} = require('./handler');
 
 const routes = [
   {
@@ -10,6 +14,12 @@ const routes = [
     method: 'GET',
     path: '/books',
     handler: getAllBooksHandler,
+  },
+
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookDetailById,
   },
 ];
 
